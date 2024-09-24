92e498e6f021a088a7dc9dba2683584fac54241c
sachin <sk800968@gmail.com>
2024-09-24 10:05:03 +0530

    added CoinTable with using react-quary

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,65 +1,67 @@
-import React, { useEffect } from 'react'
-import { fetchCoinData } from '../../services/fetchCoinData'
-
+import React, { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
+import { useQuery } from "react-query";
 
 const CoinTable = () => {
 
 
 
-  useEffect(()=>{
-    fetchCoinData()
-  })
+  const { data, isLoading, isError, error} = useQuery(['coins'], () => fetchCoinData('usd'), {
+      // retry: 2,
+      // retryDelay: 1000,
+      cacheTime: 1000 * 60 * 2,
+      staleTime: 1000 * 60 * 2,
+  });
 
+
+  console.log(data);
+  
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data</div>;
+
+
+
+  
   return (
     <div className="overflow-x-auto">
-  <table className="table w-[80vw] mx-auto mt-10">
-    {/* head */}
-    <thead className='h-[4vw] bg-yellow-500  text-black'>
-      <tr className='text-[1.2vw]  '>
-      
-        <th>Coin</th>
-        <th>Price</th>
-        <th>24h Change</th>
-        <th>Mrket Cap</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-       
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-                <img
-                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                  alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div>
-          </div>
-        </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br />
-          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-        </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-        
-      </tr>
+      <table className="table w-[80vw] mx-auto mt-10">
+        {/* head */}
+        <thead className="h-[4vw] bg-yellow-500  text-black">
+          <tr className="text-[1.2vw]  ">
+            <th>Coin</th>
+            <th>Price</th>
+            <th>24h Change</th>
+            <th>Mrket Cap</th>
+          </tr>
+        </thead>
 
-    </tbody>
- 
-  </table>
-  
-</div>
-  )
-}
+        <tbody>
+          {/* Map through fetched coin data */}
+          {data?.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img src={coin.image} alt={coin.name} />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">{coin.name}</div>
+                    <div className="text-sm opacity-50">{coin.symbol.toUpperCase()}</div>
+                  </div>
+                </div>
+              </td>
+              <td>${coin.current_price.toFixed(2)}</td>
+              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              <td>${coin.market_cap.toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default CoinTable
+export default CoinTable;
--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -2,11 +2,14 @@
 import axiosInstance from "../Utils/axiosInstance";
 
 
-export async function fetchCoinData() {
+export async function fetchCoinData(currency) {
+    
+
+
     try {
-        const response = await axiosInstance.get('/coins/markets?vs_currency=usd');
-        console.log(response.data);
-        return response;
+        const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}`);
+        // console.log(response);
+        return response.data;
     } catch(error) {
         console.log(error);
         return error;
